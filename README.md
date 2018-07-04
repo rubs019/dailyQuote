84384f425fa9e33e97aa4114d28b5e30d9cd407c
rubz019 <ruben.desert@eemi.com>
2018-07-05 00:50:36 +0200

    configuration redis and reorganize folder

---
+++ README.md
@@ -0,0 +1,9 @@
+# DayliQuote
+
+A simple API that send inspirating quotes
+
+- TODO :
+Module REDIS:
+
+Module CRON :
+- Add A cron task
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { getQuote, saveQuote } = require('../lib/redis')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  const quote = getQuote()
-  res.render('index', { title: 'Express' });
+  const quote = {
+      message: 'C - Is both...',
+      author: 'Chapman',
+      isValid: false
+  }
+
+  saveQuote(quote)
+      .then(() => getQuote())
+      .then((response) => {
+        console.log(response)
+          return res
+              .status(response.statusCode)
+              .render('index', { title: response.quote.message})
+      })
+      .catch((error) => {
+          console.log()
+          return res
+              .status(error.statusCode)
+              .render('index', { title: error.message })
+      })
 });
 
 module.exports = router;
