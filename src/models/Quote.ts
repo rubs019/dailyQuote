2ea595c22f7b0d2e50b30d6901a0fdf55ec45422
rubs019 <ruben.desert@eemi.com>
2018-07-16 17:26:13 +0200

    update quote model

--- src/models/Quote.ts
+++ src/models/Quote.ts
@@ -3,12 +3,14 @@ export class QuoteModel {
     constructor(msg, name = '', date = null, isAlreadyShow) {
         this.name = name
         this.msg = msg
-        this.date = date
         this.isAlreadyShow = isAlreadyShow
     }
 
     name?: string
     msg: string
-    date?: Date | null
     isAlreadyShow: boolean
+    born?: string
+    died?: string
+    nationality?: string
+    job?: string
 }
